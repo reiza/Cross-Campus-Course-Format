962c3e80d12956d58b14a6601dc1be6fb45f3d94
Reiza Haniff <User1@Ellie.local>
2017-02-16 13:42:55 -0400

    Version bump

--- uwishared/version.php
+++ uwishared/version.php
@@ -8,6 +8,6 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017021502;        	 // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2017021503;        	 // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013000000;			     // Requires this Moodle version
 $plugin->component = 'format_uwishared';
