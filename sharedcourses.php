a5fd7eb43c736d77a29a04bdfc8021fc308bb9b7
Reiza Haniff <reiza@users.noreply.github.com>
2017-02-21 09:18:38 -0400

    Update sharedcourses.php

--- sharedcourses.php
+++ sharedcourses.php
@@ -1,13 +1,13 @@
 <?php
-	require_once('config.php');
-	require_once($CFG->libdir.'/moodlelib.php');
+require_once('config.php');
+require_once($CFG->libdir.'/moodlelib.php');
 
-	$courses = false;
+$courses = false;
 
-  $sql = "SELECT  shortname, fullname, idnumber, visible
+$sql = "SELECT  shortname, fullname, idnumber, visible
   		FROM {course} c
   		WHERE c.idnumber <> ''";
-  $courses = $DB->get_records_sql($sql);
+$courses = $DB->get_records_sql($sql);
 
-  header('Content-Type: application/json');
-  echo json_encode($courses);
+header('Content-Type: application/json');
+echo json_encode($courses);
