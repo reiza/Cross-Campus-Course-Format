183d2ef5394db3ea44b32b8cffd5cad9e24207c9
Reiza Haniff <User1@Ellie.local>
2017-02-22 11:15:20 -0400

    Version bump

--- uwishared/tool/enrol.php
+++ uwishared/tool/enrol.php
@@ -14,17 +14,6 @@ $o = new StdClass();
 $o->username = $userid;
 $o->xrns = array();
 
-// missing role id
-$sql = "SELECT cfo.value as xrn
-		FROM {user} u
-		  JOIN {user_enrolments} ua ON u.id=ua.userid
-          JOIN {enrol} e ON ua.enrolid = e.id
-          -- JOIN {course} c ON e.courseid = c.id
-		  JOIN {course_format_options} cfo ON e.courseid = cfo.courseid
-          WHERE u.username = ?
-          AND cfo.name = 'smicourseid'";
-
-
 $sql = "SELECT
           u.username,
           c.shortname as 'remotecourseshortname',
@@ -43,20 +32,9 @@ $sql = "SELECT
                 AND cfo.name='smicourseid'
 								WHERE u.username = ?";
 
-
-
-
 $enrolment = $DB->get_records_sql($sql, array($userid));
 
-// if ($enrolment) {
-//
-// 	foreach ($enrolment as $key => $value) {
-// 		$o->xrns[$value->xrn] = '5';
-// 	}
-// }
-
 header('Content-Type: application/json');
-echo json_encode($o);
-
+echo json_encode($enrolment);
 
 ?>
