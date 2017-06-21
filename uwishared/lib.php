1e2c9fe6207f34ec912fa709aefc44210a030b5c
Reiza Haniff <User1@Kyra.local>
2017-06-20 22:05:39 -0400

    indentation

--- uwishared/lib.php
+++ uwishared/lib.php
@@ -10,73 +10,56 @@ require_once($CFG->dirroot. '/course/format/lib.php');
 class format_uwishared extends format_base {
 
     public function get_view_url($section, $options = array()) {
-     return null;
-  }
+        return null;
+    }
 
     public function extend_course_navigation($navigation, navigation_node $node) { }
 
     public function get_default_blocks() {
-     return array(
-        BLOCK_POS_LEFT => array(),
-        BLOCK_POS_RIGHT => array()
-     );
-  }
+        return array(
+            BLOCK_POS_LEFT => array(),
+            BLOCK_POS_RIGHT => array()
+        );
+    }
 
 
     public function course_format_options($foreditform = false) {
-     $baseUrl = get_config('format_uwishared')->smiurl;
-     $sc = array();
-     $json = download_file_content($baseUrl.'/sharedcourses.php');
-     $jsonData = (array) json_decode($json);
+        $baseUrl = get_config('format_uwishared')->smiurl;
+        $sc = array();
+        $json = download_file_content($baseUrl.'/sharedcourses.php');
+        $jsonData = (array) json_decode($json);
 
-     if($jsonData){
-        foreach ($jsonData as $key => $value) {
-          $sc[$value->idnumber]=$value->fullname . ' / XRN: ' . $value->idnumber;
+        if($jsonData){
+            foreach ($jsonData as $key => $value) {
+                $sc[$value->idnumber]=$value->fullname . ' / XRN: ' . $value->idnumber;
+            }
         }
-     }
 
 
-     static $courseformatoptions = false;
-     if ($courseformatoptions === false) {
-        $courseformatoptions = array(
-          'smicourseid' => array(
-             'default' => '',
-             'type' => PARAM_TEXT,
-          )
-        );
-     }
+        static $courseformatoptions = false;
+        
+        if ($courseformatoptions === false) {
+            $courseformatoptions = array(
+            'smicourseid' => array(
+                'default' => '',
+                'type' => PARAM_TEXT,
+            ));
+        }
 
-     if ($foreditform && !isset($courseformatoptions['smicourseid']['label'])) {
-        $courseformatoptionsedit = array(
-          'smicourseid' => array(
-             'label' => new lang_string('smicourseid', 'format_uwishared'),
-             'help' => 'smicourseid',
-             'help_component' => 'format_uwishared',
-             'element_type' => 'select',
-             'element_attributes' => array(
-                $sc
-             ),
-          )
-        );
-        $courseformatoptions = array_merge_recursive($courseformatoptions, $courseformatoptionsedit);
-     }
-     return $courseformatoptions;
-  }
+        if ($foreditform && !isset($courseformatoptions['smicourseid']['label'])) {
+            $courseformatoptionsedit = array(
+                'smicourseid' => array(
+                    'label' => new lang_string('smicourseid', 'format_uwishared'),
+                    'help' => 'smicourseid',
+                    'help_component' => 'format_uwishared',
+                    'element_type' => 'select',
+                    'element_attributes' => array($sc),
+            ));
+            $courseformatoptions = array_merge_recursive($courseformatoptions, $courseformatoptionsedit);
+        }
+        return $courseformatoptions;
+    }
 
-    public function page_set_course(moodle_page $page) {
-//      global $CFG, $COURSE;
-//      require_once($CFG->dirroot. '/course/format/uwishared/tool/crypto.php');
-//      $baseUrl = get_config('format_uwishared')->smiurl;
-// 
-//      $course = course_get_format($COURSE)->get_course();
-//      $package = new CryptoForUWISharedCourse();
-//      $param = substr(uniqid(),-1);
-// 
-//      $redirectUrl = $baseUrl ."/auth/ocauth/acs.php?$param=" . $package->wrapCourse($course);
-//      if (!is_siteadmin()) {
-//         redirect($redirectUrl);
-//      }
-	}
-
-
+    public function page_set_course(moodle_page $page) { }
+    
 }
--- uwishared/renderer.php
+++ uwishared/renderer.php
@@ -8,79 +8,78 @@ defined('MOODLE_INTERNAL') || die();
 class format_uwishared_renderer extends plugin_renderer_base {
 
      public function display($course) {
-		global $CFG;
+        global $CFG;
 
-		$courserenderer = $this->page->get_renderer('core', 'course');
-		
-		$baseUrl = get_config('format_uwishared')->smiurl;
-		$campusid = get_config('format_uwishared')->smimappingcampusid;
-		$course = $this->get_uwi_shared_exchange_data($course->smicourseid, $campusid);
-		$param = substr(uniqid(),-1);
+        $courserenderer = $this->page->get_renderer('core', 'course');
+        
+        $baseUrl = get_config('format_uwishared')->smiurl;
+        $campusid = get_config('format_uwishared')->smimappingcampusid;
+        $course = $this->get_uwi_shared_exchange_data($course->smicourseid, $campusid);
+        $param = substr(uniqid(),-1);
 
-		$package = new crypto_for_uwi_shared();
-		
-		$redirectUrl = $baseUrl . "/auth/ocauth/acs.php?$param=" . $package->wrap($course);
+        $package = new crypto_for_uwi_shared();
+        
+        $redirectUrl = $baseUrl . "/auth/ocauth/acs.php?$param=" . $package->wrap($course);
 
-		if (!is_siteadmin() && strlen($redirectUrl) < 2084) {
-			redirect($redirectUrl);
-		} else if (strlen($redirectUrl) > 208) {
-			$output = '<form method="post" action="'. $baseUrl . '/auth/ocauth/acs.php"><input type="hidden" name="p' . $param . '" value = "' . $package->wrap($course) . '"/><button name="submit" id="submit" type="submit" class="btn btn-primary" data-loading-text="Going to course...">Go to the shared course</button></form>';
-		} else {
-			$output = '<a class="btn btn-lg btn-primary" style="margin:20px;color:#fff" href="' . $redirectUrl . '">Go to the shared course</a>';
-		}
-		return $output;
+        if (!is_siteadmin() && strlen($redirectUrl) < 2084) {
+            redirect($redirectUrl);
+        } else if (strlen($redirectUrl) > 208) {
+            $output = '<form method="post" action="'. $baseUrl . '/auth/ocauth/acs.php"><input type="hidden" name="p' . $param . '" value = "' . $package->wrap($course) . '"/><button name="submit" id="submit" type="submit" class="btn btn-primary" data-loading-text="Going to course...">Go to the shared course</button></form>';
+        } else {
+            $output = '<a class="btn btn-lg btn-primary" style="margin:20px;color:#fff" href="' . $redirectUrl . '">Go to the shared course</a>';
+        }
+        return $output;
      }
      
-	public function get_uwi_shared_exchange_data($courseid, $campusid) {
-		global $CFG;
-		$data = new StdClass();
-		$data->a = $this->get_uwi_shared_user();
-		$data->b = $campusid;
-		$data->c = $courseid;
-		$data->d = $CFG->wwwroot;
-		$data->e = $this->get_uwi_shared_enrol();
-		$data->t = time();
+    public function get_uwi_shared_exchange_data($courseid, $campusid) {
+        global $CFG;
+        $data = new StdClass();
+        $data->a = $this->get_uwi_shared_user();
+        $data->b = $campusid;
+        $data->c = $courseid;
+        $data->d = $CFG->wwwroot;
+        $data->e = $this->get_uwi_shared_enrol();
+        $data->t = time();
 
-		return $data;
-  	
-	}
+        return $data;
+    }
   
     private function get_uwi_shared_user(){
-   		global $USER, $DB;
+           global $USER, $DB;
         return $DB->get_record('user', array('id'=>$USER->id), 'username AS a, idnumber AS b, firstname AS c, lastname  AS d, email AS e, institution AS f, department AS g, city AS h, country AS i, timezone AS j');
     }
     
     private function get_uwi_shared_enrol() {
-   		global $USER, $DB;
-		$enrolment = FALSE;
-		$o->xrns = array();
+           global $USER, $DB;
+        $enrolment = FALSE;
+        $o->xrns = array();
 
-		$sql = "SELECT
-					 u.username,
-					 c.shortname AS remotecourseshortname,
-					 r.id AS remoteroleid,
-					 r.name AS remoterolename,
-					 r.shortname AS remoteroleshortname,
-					 cfo.value AS smicourseid
-				  FROM
-					 {role_assignments} ra
-				  JOIN {user} u ON ra.userid = u.id
-				  JOIN {context} ctx ON ra.contextid = ctx.id
-				  JOIN {course} c ON ctx.instanceid=c.id
-				  JOIN {role} r ON ra.roleid=r.id
-				  JOIN {course_format_options} cfo
-						ON        cfo.courseid=c.id
-						    AND   cfo.name='smicourseid'
-										WHERE u.username = ?";
+        $sql = "SELECT
+                     u.username,
+                     c.shortname AS remotecourseshortname,
+                     r.id AS remoteroleid,
+                     r.name AS remoterolename,
+                     r.shortname AS remoteroleshortname,
+                     cfo.value AS smicourseid
+                  FROM
+                     {role_assignments} ra
+                  JOIN {user} u ON ra.userid = u.id
+                  JOIN {context} ctx ON ra.contextid = ctx.id
+                  JOIN {course} c ON ctx.instanceid=c.id
+                  JOIN {role} r ON ra.roleid=r.id
+                  JOIN {course_format_options} cfo
+                        ON        cfo.courseid=c.id
+                            AND   cfo.name='smicourseid'
+                                        WHERE u.username = ?";
 
-		$enrolment = $DB->get_records_sql($sql, array($USER->id));
+        $enrolment = $DB->get_records_sql($sql, array($USER->id));
 
-		if ($enrolment) {
-		  foreach ($enrolment as $user => $enrols) {
-			 $xrns[$enrols->smicourseid] = $enrols->remoteroleid;
-		  }
-		}
-		return $xrns;
+        if ($enrolment) {
+          foreach ($enrolment as $user => $enrols) {
+             $xrns[$enrols->smicourseid] = $enrols->remoteroleid;
+          }
+        }
+        return $xrns;
     }
 
 }
